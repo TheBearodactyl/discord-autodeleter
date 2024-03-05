74cb205b74946f5981733c8f1976f853367caebd
TheBearodactyl <dabewodactowluwu@gmail.com>
2024-03-05 14:25:17 -0600

    cool beans :D

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
+mod commands;
+
 use {
     serenity::{
         async_trait,
         client::{Context, EventHandler},
         framework::standard::{
-            help_commands,
-            macros::{command, group, help},
-            Args, CommandGroup, CommandResult, HelpOptions, StandardFramework,
+            macros::group,
+            StandardFramework,
         },
         model::{
             channel::Message,
@@ -17,6 +18,7 @@ use {
     },
     std::{collections::HashSet, sync::Arc},
     tokio::sync::Mutex,
+    commands::*,
 };
 
 #[group]
@@ -120,60 +122,4 @@ async fn main() {
 
     let counter_value = counter.lock().await;
     println!("Total messages processed: {}", *counter_value);
-}
-
-#[help]
-async fn my_help(
-    ctx: &Context,
-    msg: &Message,
-    args: Args,
-    help_options: &'static HelpOptions,
-    groups: &[&'static CommandGroup],
-    owners: std::collections::HashSet<serenity::model::id::UserId>,
-) -> CommandResult {
-    let _ = help_commands::with_embeds(ctx, msg, args, help_options, groups, owners).await;
-    Ok(())
-}
-
-#[command]
-async fn setrole(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
-    let role_id: RoleId = args.single()?;
-    let mut data = ctx.data.write().await;
-    let autodelete_roles = data.get_mut::<Handler>().unwrap();
-    autodelete_roles.lock().await.insert(role_id);
-    msg.channel_id
-        .say(&ctx.http, format!("Autodelete role {} added.", role_id))
-        .await?;
-    Ok(())
-}
-
-#[command]
-async fn getrole(ctx: &Context, msg: &Message) -> CommandResult {
-    let data = ctx.data.read().await;
-    let autodelete_roles = data.get::<Handler>().unwrap();
-
-    let mut role_names = Vec::new();
-    for role_id in &*autodelete_roles.lock().await {
-        if let Some(guild) = ctx.cache.guild(GuildId(917057579039989770)).await {
-            if let Some(role) = guild.roles.get(role_id) {
-                role_names.push(role.name.clone());
-            }
-        }
-    }
-
-    if !role_names.is_empty() {
-        let role_list = role_names.join(", ");
-        msg.channel_id
-            .say(
-                &ctx.http,
-                format!("Current autodelete roles: {}", role_list),
-            )
-            .await?;
-    } else {
-        msg.channel_id
-            .say(&ctx.http, "No autodelete roles set.")
-            .await?;
-    }
-
-    Ok(())
 }
